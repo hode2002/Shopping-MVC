3ab7d5a87325121bae935ec37c907294ba95d6fc
hode2002 <hovande.dev.2002@gmail.com>
2023-09-24 18:50:44 +0700

    Cập nhật giao diện

--- public/js/app.js
+++ public/js/app.js
@@ -180,8 +180,8 @@ $(() => {
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 4,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
                         infinite: true,
                         prevArrow: "",
                         nextArrow: ""
@@ -190,8 +190,8 @@ $(() => {
                 {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 4,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
                         infinite: true,
                         prevArrow: "",
                         nextArrow: ""
@@ -200,8 +200,8 @@ $(() => {
                 {
                     breakpoint: 480,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 4,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
                         infinite: true,
                         prevArrow: "",
                         nextArrow: ""
